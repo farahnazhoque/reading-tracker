53e8fc198483ec9ddfc122129e06edc921bf3520
Farahnaz Hoque <fhoque@uci.edu>
2023-12-24 16:21:49 -0800

    started building the interface

--- ReadingTrack/index.js
+++ ReadingTrack/index.js
@@ -14,30 +14,48 @@ function displayBooks(data) {
     booksContainer.innerHTML = ''; // Clear existing books
 
     data.items.forEach(book => {
-        // Create elements for book cover, title, and author
-        // Append these elements to booksContainer
-        
-        // creating a container for each book
         let bookDiv = document.createElement('div');
         bookDiv.classList.add('book');
 
-        // create an image element for the book cover
+        // Create an image element for the book cover
         let coverImage = document.createElement('img');
-        coverImage.src = book.volumeInfo.imageLinks?.thumbnail;
+        coverImage.src = book.volumeInfo.imageLinks?.thumbnail || './images/default.jpg';
         coverImage.alt = book.volumeInfo.title;
-        bookDiv.appendChild(coverImage);
 
-        // creating a paragraph element for the title
+        // Create a link that will redirect to the GoodReads page of the book
+        let coverLink = document.createElement('a');
+        // Here you need to provide the actual GoodReads link for the book
+        coverLink.href = book.volumeInfo.infoLink; // This URL is from the Google Books API and often redirects to a page with more information about the book, which may include links to GoodReads or other retailers.
+        coverLink.appendChild(coverImage);
+        bookDiv.appendChild(coverLink);
+
+        // Add the title and author
         let title = document.createElement('p');
+        title.classList.add('book-title');
         title.textContent = book.volumeInfo.title;
         bookDiv.appendChild(title);
 
-        // creating a paragraph element for the author
         let author = document.createElement('p');
+        author.classList.add('book-author');
         author.textContent = book.volumeInfo.authors?.join(', ') || 'Unknown Author';
         bookDiv.appendChild(author);
 
-        // append the bookDiv to the bookContainer
+        // Create 'Add to My List' button
+        let myListButton = document.createElement('button');
+        myListButton.textContent = 'Add to My List';
+        myListButton.classList.add('my-list-button');
+        myListButton.addEventListener('click', function() {
+            addToMyList(book);
+        });
+        bookDiv.appendChild(myListButton);
+
+        // Append the bookDiv to the booksContainer
         booksContainer.appendChild(bookDiv);
     });
+}
+
+function addToMyList(book) {
+    // Implement functionality to add the book to 'mylist.html' or store in local storage/session storage
+    console.log('Added to My List:', book.volumeInfo.title); // Placeholder for actual add to list functionality
+    // You can store the book data in localStorage or send it to the server or simply redirect to 'mylist.html' with book details
 }
